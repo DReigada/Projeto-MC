136f5e44103fabb266512b3c1682851945318e22
DReigada <daniel.reigada@gmail.com>
2015-12-13 16:14:00 +0000

    Add the exercise 1 of Grupo3

---
+++ Grupo3/Legendre.m
@@ -0,0 +1,8 @@
+function y = Legendre( n, X )
+%Legendre Returns the value of the legendre
+%function of order n in the given point
+
+P = legendre(n, X);
+y = P(1);
+end
+
---
+++ Grupo3/test.m
@@ -0,0 +1,14 @@
+%create the directory if it did not exist already
+if ~exist('./images','dir')
+    mkdir('images');
+end
+
+%save the plots of the legendre polynomials of order 1:10
+for n = 1:10
+    fig = figure('Visible','off');
+    leg = @(x) Legendre(n,x);   %the polynimial to plot
+    fplot(leg, [-1, 1]);    %plot the polynomial
+    hold on;
+    fplot('0', [-1, 1]);    %plot the constant function y = 0
+    saveas(fig, sprintf('images\\legendre-ord%d.jpg', n));  %save the image
+end 
